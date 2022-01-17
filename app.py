b1457e80869e3f242e5de87f82845b89a92ae74c
SrinathBala <92857200+SrinathBala@users.noreply.github.com>
2022-01-17 11:15:51 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask
+import logging
+app = Flask(__name__)
+logging.basicConfig(filename='dev.log', level=logging.INFO)
+@app.route('/')
+def hello():
+    return 'hello world Srinath!'
+
+if __name__== "__main__":
+    app.run(host='0.0.0.0', port=80)
